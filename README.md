12563c91a30cfd309428cb8b1f358c7e8f02dbcd
yonggyo1125 <yonggyo00@kakao.com>
2024-04-11 16:30:11 +0900

    공통 스타일, 공통 글자 크기, 공통 색상

--- README.md
+++ README.md
@@ -216,4 +216,4 @@ root.render(
 - 공통 폰트
 - 스타일 초기화
 - 기준 폰트 사이즈 : src/styles/fontSize.js / small, normal, medium, big, extraBig
-- 기준 컬러 : 
+- 기준 컬러 : Primary, Secondary, Success, Danger, Warning, Info, Light, Dark
---
+++ src/styles/color.js
@@ -0,0 +1,21 @@
+export const color = {
+  primary: '#007bff',
+  secondary: '#6c757d',
+  success: '#28a745',
+  danger: '#dc3545',
+  warning: '#ffc107',
+  info: '#17a2b8',
+  light: '#f8f9fa',
+  dark: '#343a40',
+};
+
+export const buttonColor = {
+  primary: [color.primary, '#fff'],
+  secondary: [color.secondary, '#fff'],
+  success: [color.success, '#fff'],
+  danger: [color.danger, '#fff'],
+  warning: [color.warning, '#212529'],
+  info: [color.info, '#fff'],
+  light: [color.light, '#212529'],
+  dark: [color.dark, '#fff'],
+};
