db2f9b38c865b3318e18280e9c480c8cfefac258
yonggyo1125 <yonggyo00@kakao.com>
2024-04-12 16:40:57 +0900

    .

--- src/outlines/Header.js
+++ src/outlines/Header.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
+
+const HeaderBox = styled.header``;
 
 const Header = () => {
-  return <header>헤더</header>;
+  const { t } = useTranslation();
+  
+  return (
+    <HeaderBox>
+      <section>
+        <Link to="/member/join"></Link>
+      </section>
+    </HeaderBox>
+  );
 };
 
 export default React.memo(Header);
