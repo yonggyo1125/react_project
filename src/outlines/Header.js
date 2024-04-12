7f44765ff3df52784760e9c2d2dac5aa939bedd0
yonggyo1125 <yonggyo00@kakao.com>
2024-04-12 16:44:45 +0900

    .

--- src/outlines/Header.js
+++ src/outlines/Header.js
@@ -3,15 +3,18 @@ import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 
-const HeaderBox = styled.header``;
+const HeaderBox = styled.header`
+  
+`;
 
 const Header = () => {
   const { t } = useTranslation();
-  
+
   return (
     <HeaderBox>
-      <section>
-        <Link to="/member/join"></Link>
+      <section className="site-top">
+        <Link to="/member/join">{t('회원가입')}</Link>
+        <Link to="/member/login">{t('로그인')}</Link>
       </section>
     </HeaderBox>
   );
