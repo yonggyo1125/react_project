6aabcc3b4a92c7f6e85fc8fd813bc1ac6825dd36
yonggyo1125 <yonggyo00@kakao.com>
2024-04-12 17:03:36 +0900

    .

--- src/outlines/Header.js
+++ src/outlines/Header.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
+import classNames from 'classnames';
+import fontSize from '../styles/fontSize';
 
 const HeaderBox = styled.header`
-  
+  .site-top {
+    background: #f8f8f8;
+    border-bottom: 1px solid #d5d5d5;
+    height: 35px;
+
+    div {
+      text-align: right;
+
+      a {
+        display: inline-block;
+        line-height: 34px;
+        margin-left: 10px;
+        font-size: ${fontSize.normal};
+      }
+    }
+  }
 `;
 
 const Header = () => {
@@ -13,8 +30,15 @@ const Header = () => {
   return (
     <HeaderBox>
       <section className="site-top">
-        <Link to="/member/join">{t('회원가입')}</Link>
-        <Link to="/member/login">{t('로그인')}</Link>
+        <div className="layout-width">
+          <NavLink
+            to="/member/join"
+            className={({ isActive }) => classNames({ on: isActive })}
+          >
+            {t('회원가입')}
+          </NavLink>
+          <NavLink to="/member/login">{t('로그인')}</NavLink>
+        </div>
       </section>
     </HeaderBox>
   );
