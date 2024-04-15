e07f292e9ce971a36429a86b58f7bc62377b401a
yonggyo1125 <yonggyo00@kakao.com>
2024-04-15 16:46:05 +0900

    .

--- src/outlines/MainMenu.js
+++ src/outlines/MainMenu.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import styled from 'styled-components';
+import classNames from 'classnames';
+import { NavLink } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
+
+const MenuBox = styled.nav``;
 
 const MainMenu = () => {
-  return <nav>메뉴</nav>;
+  const { t } = useTranslation();
+
+  return (
+    <MenuBox>
+      <div className="layout-width">
+        <NavLink
+          to="/news"
+          className={({ isActive }) => classNames({ on: isActive })}
+        >
+          {t('뉴스')}
+        </NavLink>
+      </div>
+    </MenuBox>
+  );
 };
 
 export default React.memo(MainMenu);
