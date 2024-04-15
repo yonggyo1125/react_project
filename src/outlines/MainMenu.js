ee5359662293d501c71092081a24014695c3e32f
yonggyo1125 <yonggyo00@kakao.com>
2024-04-15 16:51:01 +0900

    .

--- src/outlines/MainMenu.js
+++ src/outlines/MainMenu.js
@@ -3,8 +3,22 @@ import styled from 'styled-components';
 import classNames from 'classnames';
 import { NavLink } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
+import { color } from '../styles/color';
 
-const MenuBox = styled.nav``;
+const { dark, primary, light } = color;
+
+const MenuBox = styled.nav`
+  background: ${dark};
+
+  div {
+    display: flex;
+    height: 50px;
+
+    a {
+        color: ${light};
+    }
+  }
+`;
 
 const MainMenu = () => {
   const { t } = useTranslation();
