db1458b52d9374c1eaa3f41355bf875fe2260948
yonggyo1125 <yonggyo00@kakao.com>
2024-04-15 17:36:22 +0900

    .

---
+++ src/member/components/JoinForm.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const JoinForm = () => {
+  return <h1>회원가입 양식...</h1>;
+};
+
+export default React.memo(JoinForm);
--- src/member/pages/Join.js
+++ src/member/pages/Join.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import { Helmet } from 'react-helmet-async';
+import { useTranslation } from 'react-i18next';
+import JoinForm from '../components/JoinForm';
 
 const Join = () => {
-  return <h1>회원 가입페이지</h1>;
+  const { t } = useTranslation();
+
+  return (
+    <>
+      <Helmet>
+        <title>{t('회원가입')}</title>
+      </Helmet>
+      <h1>{t('회원가입')}</h1>
+      <JoinForm />
+    </>
+  );
 };
 
 export default React.memo(Join);
