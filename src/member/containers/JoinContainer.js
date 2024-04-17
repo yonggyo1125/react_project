c42e228896e9b35469f645cd2d93e7c4c998bf0c
yonggyo1125 <yonggyo00@kakao.com>
2024-04-17 15:51:41 +0900

    .

--- src/member/containers/JoinContainer.js
+++ src/member/containers/JoinContainer.js
@@ -3,9 +3,33 @@ import { useTranslation } from 'react-i18next';
 import JoinForm from '../components/JoinForm';
 
 const JoinContainer = () => {
-    const [form, setForm] = useState({});
+  const [form, setForm] = useState({
+    agree: false,
+  });
 
-    const { t } = useTranslation();
+  const { t } = useTranslation();
+
+  /**
+   * 회원 가입 처리
+   *
+   * 1. 데이터 검증
+   *    1) 필수 항목 체크 - 이메일, 비밀번호, 비밀번호 확인, 회원명, 약관동의
+   *    2) 이메일 중복 여부, 이메일 형식 체크
+   *    3) 비밀번호 복잡성 체크
+   *    4) 비밀번호와 비밀번호 확인 일치 여부
+   *
+   * 2. 가입 처리 - 영구 저장
+   * 3. 로그인 페이지 이동
+   */
+  const onSubmit = useCallback((e) => {
+    e.preventDefault();
+  }, []);
+
+  const onChange = useCallback((e) => {
+    const name = e.target.name;
+    const value = e.target.value.trim();
+    setForm((form) => ({ ...form, [name]: value }));
+  }, []);
 
   return <JoinForm />;
 };
