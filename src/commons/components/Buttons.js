1d8e86ae5d02e471fbf7b4f8b6ef86133e003fd6
yonggyo1125 <yonggyo00@kakao.com>
2024-04-18 11:45:17 +0900

    .

--- src/commons/components/Buttons.js
+++ src/commons/components/Buttons.js
@@ -1,14 +1,31 @@
 import styled, { css } from 'styled-components';
 import { buttonColor } from '../../styles/color';
 import fontSize from '../../styles/fontSize';
-const { big } = fontSize;
+const { big, medium } = fontSize;
+const commonStyle = css`
+  width: 100%;
+  border-radius: 3px;
+  cursor: pointer;
+`;
+
+export const MidButton = styled.button`
+  font-size: ${medium};
+  height: 40px;
+  ${commonStyle}
+
+  ${({ color }) =>
+    buttonColor[color] &&
+    css`
+      background: ${buttonColor[color][0]};
+      color: ${buttonColor[color][1]};
+      border: 1px solid ${buttonColor[color][2]};
+    `}
+`;
 
 export const BigButton = styled.button`
   font-size: ${big};
   height: 45px;
-  width: 100%;
-  border-radius: 3px;
-  cursor: pointer;
+  ${commonStyle}
 
   ${({ color }) =>
     buttonColor[color] &&
