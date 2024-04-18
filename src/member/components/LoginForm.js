5f2f002d1ab9cad9a6546e3a866c2b40d80589bc
yonggyo1125 <yonggyo00@kakao.com>
2024-04-18 11:47:07 +0900

    .

--- src/member/components/LoginForm.js
+++ src/member/components/LoginForm.js
@@ -4,6 +4,7 @@ import { useTranslation } from 'react-i18next';
 import { Link } from 'react-router-dom';
 import { FaLock, FaKey, FaUserPlus } from 'react-icons/fa';
 import InputBox from '../../commons/components/InputBox';
+import { MidButton } from '../../commons/components/Buttons';
 
 const FormBox = styled.form``;
 const LinkBox = styled.div``;
@@ -16,7 +17,9 @@ const LoginForm = () => {
       <FormBox autoComplete="off">
         <InputBox type="text" placeholder={t('이메일')} />
         <InputBox type="password" placeholder={t('비밀번호')} />
-        <button type="submit">{t('로그인')}</button>
+        <MidButton type="submit" color="primary">
+          {t('로그인')}
+        </MidButton>
       </FormBox>
       <LinkBox>
         <Link to="/member/find_id">
