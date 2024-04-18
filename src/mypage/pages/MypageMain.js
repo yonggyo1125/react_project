71102373d96200b379514a2e405c4a26bb746e7f
yonggyo1125 <yonggyo00@kakao.com>
2024-04-18 16:50:50 +0900

    .

--- src/mypage/pages/MypageMain.js
+++ src/mypage/pages/MypageMain.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import MemberOnly from '../../commons/components/auth/MemberOnly';
 
 const MypageMain = () => {
-  return <h1>마이페이지</h1>;
+  return (
+    <MemberOnly>
+      <h1>마이페이지</h1>
+    </MemberOnly>
+  );
 };
 
 export default React.memo(MypageMain);
