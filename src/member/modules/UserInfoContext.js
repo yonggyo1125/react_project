8b921c41c76bb992ed8596078cb39d71fec04140
yonggyo1125 <yonggyo00@kakao.com>
2024-04-18 15:36:42 +0900

    .

--- src/member/modules/UserInfoContext.js
+++ src/member/modules/UserInfoContext.js
@@ -1,10 +1,35 @@
-import { createContext } from 'react';
+import { createContext, useState } from 'react';
 
 const UserInfoContext = createContext({
-  userInfo: {
-    email: 'user01@test.org',
-    name: '사용자01',
+  states: {
+    // 상태 값
+    userInfo: null,
+    isLogin: false,
+  },
+  actions: {
+    // 상태 변경 함수
+    setUserInfo: null,
+    setIsLogin: null,
   },
 });
+
+const UserInfoProvider = ({ children }) => {
+  const [userInfo, setUserInfo] = useState(null);
+  const [isLogin, setIsLogin] = useState(null);
+  const value = {
+    states: { userInfo, isLogin },
+    actions: { setUserInfo, setIsLogin },
+  };
+
+  return (
+    <UserInfoContext.Provider value={value}>
+      {children}
+    </UserInfoContext.Provider>
+  );
+};
+
+const { Consumer: UserInfoConsumer } = UserInfoContext;
+
+export { UserInfoConsumer, UserInfoProvider };
 
 export default UserInfoContext;
