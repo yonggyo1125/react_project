6de50529631e8b0f25743f4e910132123f9a094a
yonggyo1125 <yonggyo00@kakao.com>
2024-04-18 11:01:16 +0900

    .

--- src/member/pages/Login.js
+++ src/member/pages/Login.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import { Helmet } from 'react-helmet-async';
+import { useTranslation } from 'react-i18next';
+import styled from 'styled-components';
+import LoginContainer from '../containers/LoginContainer';
+const OuterBox = styled.div``;
 
 const Login = () => {
-  return <h1>로그인 페이지</h1>;
+  const { t } = useTranslation();
+  return (
+    <>
+      <LoginContainer />
+    </>
+  );
 };
 
 export default React.memo(Login);
