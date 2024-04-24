09a57007af34a10a1c4ac6dfec3f2f881fad3d44
yonggyo1125 <yonggyo00@kakao.com>
2024-04-24 20:36:05 +0900

    .

--- src/news/components/NewsItems.js
+++ src/news/components/NewsItems.js
@@ -1,6 +1,27 @@
 import React from 'react';
-const NewsItems = () => {
-  return <h1>뉴스 목록</h1>;
+import styled from 'styled-components';
+
+const NewsItems = ({ items }) => {
+  return (
+    <ul>
+      {items.map((item) => (
+        <NewsItem key={item.url} item={item} />
+      ))}
+    </ul>
+  );
+};
+
+const NewsItem = ({ item }) => {
+  const { urlToImage, url, title, description } = item;
+  return (
+    <li>
+      <img src={urlToImage} alt={title} />
+      <div className="content-box">
+        <div className="title">{title}</div>
+        <div className="description">{description}</div>
+      </div>
+    </li>
+  );
 };
 
 export default React.memo(NewsItems);
--- src/news/containers/NewsContainer.js
+++ src/news/containers/NewsContainer.js
@@ -4,35 +4,35 @@ import NewsCategory from '../components/NewsCategory';
 import NewsItems from '../components/NewsItems';
 
 const NewsContainer = () => {
-    const [items, setItems] = useState([
-        {
-          title: '제목',
-          description: '내용',
-          urlToImage: 'https://www.imdak.com/web/upload/wp/167417/logo.toppc.png',
-          url: 'https://www.naver.com',
-        },
-        {
-          title: '제목',
-          description: '내용',
-          urlToImage: 'https://www.imdak.com/web/upload/wp/167417/logo.toppc.png',
-          url: 'https://www.naver.com',
-        },
-        {
-          title: '제목',
-          description: '내용',
-          urlToImage: 'https://www.imdak.com/web/upload/wp/167417/logo.toppc.png',
-          url: 'https://www.naver.com',
-        },
-        {
-          title: '제목',
-          description: '내용',
-          urlToImage: 'https://www.imdak.com/web/upload/wp/167417/logo.toppc.png',
-          url: 'https://www.naver.com',
-        },
-      ]);
-    
-      useEffect(() => {
-        /*
+  const [items, setItems] = useState([
+    {
+      title: '제목',
+      description: '내용',
+      urlToImage: 'https://www.imdak.com/web/upload/wp/167417/logo.toppc.png',
+      url: 'https://www.naver.com',
+    },
+    {
+      title: '제목',
+      description: '내용',
+      urlToImage: 'https://www.imdak.com/web/upload/wp/167417/logo.toppc.png',
+      url: 'https://www.naver.com',
+    },
+    {
+      title: '제목',
+      description: '내용',
+      urlToImage: 'https://www.imdak.com/web/upload/wp/167417/logo.toppc.png',
+      url: 'https://www.naver.com',
+    },
+    {
+      title: '제목',
+      description: '내용',
+      urlToImage: 'https://www.imdak.com/web/upload/wp/167417/logo.toppc.png',
+      url: 'https://www.naver.com',
+    },
+  ]);
+
+  useEffect(() => {
+    /*
         const url =
           'https://newsapi.org/v2/top-headlines?country=kr&apiKey=6593897e9b3d40178104e2cb158f2866';
     
@@ -40,14 +40,14 @@ const NewsContainer = () => {
           .then((res) => res.json())
           .then((json) => console.log(json));
           */
-      }, []);
+  }, []);
 
-    return (
-        <>
-            <NewsCategory />
-            <NewsItems />
-        </>
-    );
+  return (
+    <>
+      <NewsCategory />
+      <NewsItems items={items} />
+    </>
+  );
 };
 
 export default React.memo(NewsContainer);
