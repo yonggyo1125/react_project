d4617ac937ede14596a7001889ccb4c88e91a28d
yonggyo1125 <yonggyo00@kakao.com>
2024-04-24 20:58:05 +0900

    .

--- src/news/containers/NewsContainer.js
+++ src/news/containers/NewsContainer.js
@@ -3,12 +3,44 @@ import React, { useState, useEffect, useCallback } from 'react';
 import NewsCategory from '../components/NewsCategory';
 import NewsItems from '../components/NewsItems';
 
+const categories = [
+  {
+    name: 'all',
+    text: '전체보기',
+  },
+  {
+    name: 'business',
+    text: '경제',
+  },
+  {
+    name: 'entertainment',
+    text: '방송/연예',
+  },
+  {
+    name: 'health',
+    text: '건강',
+  },
+  {
+    name: 'science',
+    text: '과학',
+  },
+  {
+    name: 'sports',
+    text: '스포츠',
+  },
+  {
+    name: 'technology',
+    text: '기술',
+  },
+];
+
 const NewsContainer = () => {
   const [items, setItems] = useState([]);
+  const [category, setCategory] = useState('all');
 
   useEffect(() => {
-    const url =
-      'https://newsapi.org/v2/top-headlines?country=kr&apiKey=6593897e9b3d40178104e2cb158f2866';
+    const addQs = category === 'all' ? '' : `&category=${category}`;
+    const url = `https://newsapi.org/v2/top-headlines?country=kr${addQs}&apiKey=6593897e9b3d40178104e2cb158f2866`;
 
     fetch(url)
       .then((res) => res.json())
