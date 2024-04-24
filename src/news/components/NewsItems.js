023ca756c282155a7ade9c3a26ab97af15ec58a2
yonggyo1125 <yonggyo00@kakao.com>
2024-04-24 20:46:26 +0900

    .

--- src/news/components/NewsItems.js
+++ src/news/components/NewsItems.js
@@ -1,5 +1,31 @@
 import React from 'react';
 import styled from 'styled-components';
+import { useNavigate } from 'react-router-dom';
+import fontSize from '../../styles/fontSize';
+const { big } = fontSize;
+
+const NewItemBox = styled.li`
+  display: flex;
+
+  img {
+    width: 280px;
+    margin-right: 30px;
+  }
+
+  .content-box {
+    flex-grow: 1;
+
+    .title {
+      font-size: ${big};
+      font-weight: 700;
+      margin-bottom: 20px;
+    }
+  }
+
+  & + & {
+    margin-top: 30px;
+  }
+`;
 
 const NewsItems = ({ items }) => {
   return (
@@ -12,15 +38,17 @@ const NewsItems = ({ items }) => {
 };
 
 const NewsItem = ({ item }) => {
+  const navigate = useNavigate();
   const { urlToImage, url, title, description } = item;
+
   return (
-    <li>
+    <NewItemBox onClick={() => navigate(url)}>
       <img src={urlToImage} alt={title} />
       <div className="content-box">
         <div className="title">{title}</div>
         <div className="description">{description}</div>
       </div>
-    </li>
+    </NewItemBox>
   );
 };
 
