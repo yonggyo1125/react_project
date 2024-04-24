d288cc64758db43471f4f7dd34f1f20c76b8cbf9
yonggyo1125 <yonggyo00@kakao.com>
2024-04-24 20:49:00 +0900

    .

--- src/news/containers/NewsContainer.js
+++ src/news/containers/NewsContainer.js
@@ -4,42 +4,15 @@ import NewsCategory from '../components/NewsCategory';
 import NewsItems from '../components/NewsItems';
 
 const NewsContainer = () => {
-  const [items, setItems] = useState([
-    {
-      title: '제목',
-      description: '내용',
-      urlToImage: 'https://www.imdak.com/web/upload/wp/167417/logo.toppc.png',
-      url: 'https://www.naver.com',
-    },
-    {
-      title: '제목',
-      description: '내용',
-      urlToImage: 'https://www.imdak.com/web/upload/wp/167417/logo.toppc.png',
-      url: 'https://www.naver.com',
-    },
-    {
-      title: '제목',
-      description: '내용',
-      urlToImage: 'https://www.imdak.com/web/upload/wp/167417/logo.toppc.png',
-      url: 'https://www.naver.com',
-    },
-    {
-      title: '제목',
-      description: '내용',
-      urlToImage: 'https://www.imdak.com/web/upload/wp/167417/logo.toppc.png',
-      url: 'https://www.naver.com',
-    },
-  ]);
+  const [items, setItems] = useState([]);
 
   useEffect(() => {
-    /*
-        const url =
-          'https://newsapi.org/v2/top-headlines?country=kr&apiKey=6593897e9b3d40178104e2cb158f2866';
-    
-        fetch(url)
-          .then((res) => res.json())
-          .then((json) => console.log(json));
-          */
+    const url =
+      'https://newsapi.org/v2/top-headlines?country=kr&apiKey=6593897e9b3d40178104e2cb158f2866';
+
+    fetch(url)
+      .then((res) => res.json())
+      .then((json) => setItems(json.articles));
   }, []);
 
   return (
