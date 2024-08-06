da39fae1b836234a97231ae75b2b8414e95436c5
yonggyo1125 <yonggyo00@kakao.com>
2024-08-06 19:01:27 +0900

    .

--- src/commons/libs/apiRequest.js
+++ src/commons/libs/apiRequest.js
@@ -1,28 +1,28 @@
-export default function apiRequest(url, method = 'GET', data, headers) {
-  // url - http://naver.com https://naver.com
+import axios from 'axios';
 
-  if (!/^http[s]?:/i.test(url)) {
-    // API 서버로 요청 보내는 주소인 경우
+export default function apiRequest(url, method = 'GET', data, headers) {
+  /**
+   * url - http://jsonplaceholder.. https://
+   */
+  if (!/^http[s]?/i.test(url)) {
+    // 외부 URL이 아닌 경우 - http://localhost:4000/api/v1/account
     url = process.env.REACT_APP_API_URL + url;
-    // /account  -> http://localhost:3000/api/v1/account
   }
+
+  /**
+   * axios 응답 코드가 2xx ~ 3xx 만 정상 응답 판단 
+   *       그외의 응답 코드는 예외 발생 -> 4xx 역시 오류로 판단 -> 정상 응답의 범위를 변경
+   */
 
   const options = {
-    mode: 'no-cors',
     method,
+    url,
+    validateStatus: status => status < 500, // 500 미만의 응답 코드는 정상 응답 
   };
 
-  // BODY가 있는 요청인 경우
   if (['POST', 'PUT', 'PATCH'].includes(method.toUpperCase()) && data) {
-    headers = headers ?? {};
-    headers['Content-Type'] = 'application/json';
-    options.body = JSON.stringify(data); // 요청 데이터
+    options.data = data;
   }
 
-  return new Promise((resolve, reject) => {
-    fetch(url, options)
-      .then((res) => res.json())
-      .then((json) => resolve(json))
-      .catch((err) => reject(err));
-  });
+  if (headers) options.headers = headers;
 }
