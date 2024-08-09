8260885bef4f950c0c53f9cb6ea8feb175d58608
yonggyo1125 <yonggyo00@kakao.com>
2024-08-09 15:48:20 +0900

    .

--- src/map/KakaoMap.js
+++ src/map/KakaoMap.js
@@ -7,12 +7,13 @@ const MapArea = styled.div`
   height: ${({ height }) => height ?? '38.5rem'};
 `;
 
-const KakaoMap = ({ width, height, center, zoom }) => {
+const KakaoMap = ({ width, height, center, zoom, marker }) => {
   const mapRef = useRef(null);
 
   useEffect(() => {
     const mapEl = mapRef.current;
 
+    // 지도 가운데 배치 S
     const position = new kakao.maps.LatLng(
       center?.lat ?? 37.557756188912954,
       center?.lng ?? 126.94062742683245,
@@ -21,7 +22,27 @@ const KakaoMap = ({ width, height, center, zoom }) => {
       center: position,
       level: zoom || 3,
     });
-  }, [mapRef, center, zoom]);
+    // 지도 가운데 배치 E
+
+    // 마커 출력 S
+    if (marker) {
+      let _markers = marker;
+      if (!Array.isArray(marker)) _markers = [marker];
+
+      const markers = _markers.map((m) => {
+        const { lat, lng } = m;
+        const options = {
+          position: new kakao.maps.LatLng(lat, lng),
+        };
+
+        const _marker = new kakao.maps.Marker(options);
+        _marker.setMap(map);
+
+        return _marker;
+      });
+    }
+    // 마커 출력 E
+  }, [mapRef, center, zoom, marker]);
 
   return <MapArea ref={mapRef} width={width} height={height} />;
 };
