21cb667d8515564b189654c81068e9a638f27b3d
yonggyo1125 <yonggyo00@kakao.com>
2024-08-13 21:13:10 +0900

    .

--- src/restaurant/components/ItemsBox.js
+++ src/restaurant/components/ItemsBox.js
@@ -2,7 +2,16 @@ import React from 'react';
 import styled from 'styled-components';
 
 const ItemBox = ({ item, className }) => {
-  return <li className={className}></li>;
+  const { images, rstrNm, rstrIntrcnCont } = item;
+  return (
+    <li className={className}>
+      {images && images.length > 0 && (
+        <div className="photo">
+          <img src={images[0]} alt={rstrNm} />
+        </div>
+      )}
+    </li>
+  );
 };
 
 const ItemsBox = ({ items }) => {
