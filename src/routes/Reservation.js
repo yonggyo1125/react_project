1c50f996b4dd736892a78df0727e7a15834fd538
yonggyo1125 <yonggyo00@kakao.com>
2024-08-14 21:16:35 +0900

    .

---
+++ src/routes/Reservation.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import loadable from '@loadable/component';
+
+const MainLayout = loadable(() => import('../layouts/MainLayout'));
+
+const ApplyPage = loadable(() => import('../reservation/pages/Apply'));
+const CompletionPage = loadable(() =>
+  import('../reservation/pages/Completion'),
+);
+
+const Reservation = () => {
+  return (
+    <Routes>
+      <Route path="/reservation" element={<MainLayout />}>
+        <Route path="/complete" element={<CompletionPage />} />
+        <Route path=":seq" element={<ApplyPage />} />
+      </Route>
+    </Routes>
+  );
+};
+
+export default React.memo(Reservation);
