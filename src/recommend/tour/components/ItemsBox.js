1c2f119d64cfbdfc98233819cf5dd460c655b961
yonggyo1125 <yonggyo00@kakao.com>
2024-08-14 12:51:16 +0900

    .

--- src/recommend/tour/components/ItemsBox.js
+++ src/recommend/tour/components/ItemsBox.js
@@ -1,8 +1,26 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 const ItemBox = ({ item, className }) => {
-  return <li className={className}></li>;
+  const { seq, title, photoUrl, address, description } = item;
+  const url = `/recommend/tour/${seq}`;
+  return (
+    <li className={className}>
+      <Link to={url}>
+        {photoUrl && (
+          <div className="photo">
+            <img src={photoUrl} alt={title} />
+          </div>
+        )}
+        <div className="item-content">
+          <div className="title">{title}</div>
+          <div className="address">{address}</div>
+          <div className="description">{description}</div>
+        </div>
+      </Link>
+    </li>
+  );
 };
 
 const ItemStyledBox = styled(ItemBox)``;
