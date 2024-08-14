dcaa06f55c950ae5a89c75e552e81e1a2e1f4a61
yonggyo1125 <yonggyo00@kakao.com>
2024-08-14 12:46:22 +0900

    .

--- src/recommend/tour/components/ItemsBox.js
+++ src/recommend/tour/components/ItemsBox.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const ItemBox = ({ item, className }) => {
+  return <li className={className}></li>;
+};
+
+const ItemStyledBox = styled(ItemBox)``;
 
 const ItemsBox = ({ items }) => {
-  return <></>;
+  return (
+    items.length > 0 &&
+    items.map((item) => <ItemStyledBox key={item.seq} item={item} />)
+  );
 };
 
 export default React.memo(ItemsBox);
