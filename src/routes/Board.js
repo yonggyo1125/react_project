b757b3f5d29e482221c76f6530b9666d9bbdeae4
yonggyo1125 <yonggyo00@kakao.com>
2024-08-20 10:00:37 +0900

    .

---
+++ src/routes/Board.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import loadable from '@loadable/component';
+
+const MainLayout = loadable(() => import('../layouts/MainLayout'));
+
+const WritePage = loadable(() => import('../board/pages/Write'));
+const UpdatePage = loadable(() => import('../board/pages/Update'));
+const ListPage = loadable(() => import('../board/pages/List'));
+const ViewPage = loadable(() => import('../board/pages/View'));
+
+const Board = () => {
+  return (
+    <Routes>
+      <Route path="/board" element={<MainLayout />}>
+        <Route path="write/:bid" element={<WritePage />} />
+      </Route>
+    </Routes>
+  );
+};
+
+export default React.memo(Board);
