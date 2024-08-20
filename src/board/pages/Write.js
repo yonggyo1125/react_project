768b96b7d3a1f7ae960ec8cc96de7a811f5cabaf
yonggyo1125 <yonggyo00@kakao.com>
2024-08-20 10:25:27 +0900

    .

--- src/board/pages/Write.js
+++ src/board/pages/Write.js
@@ -1,23 +1,18 @@
 import React, { useState } from 'react';
 import { Helmet } from 'react-helmet-async';
-import { useTranslation } from 'react-i18next';
 import { OuterBox } from '../../commons/components/LayoutBox';
 import { MainTitle } from '../../commons/components/TitleBox';
 
 const Write = () => {
-  const { t } = useTranslation();
-  const [board, setBoard] = useState({
-    bName: '자유게시판',
-  });
+  const [pageTitle, setPageTitle] = useState('');
 
-  const { bName } = board;
   return (
     <>
       <Helmet>
-        <title>{t(bName)}</title>
+        <title>{pageTitle}</title>
       </Helmet>
       <OuterBox>
-        <MainTitle>{t(bName)}</MainTitle>
+        <MainTitle>{pageTitle}</MainTitle>
       </OuterBox>
     </>
   );
