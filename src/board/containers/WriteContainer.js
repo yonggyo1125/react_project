dced6c87ddf0f7c65f4bf0b93bdab5b299d598bc
yonggyo1125 <yonggyo00@kakao.com>
2024-08-20 10:38:16 +0900

    .

--- src/board/containers/WriteContainer.js
+++ src/board/containers/WriteContainer.js
@@ -1,8 +1,36 @@
 import React, { useState, useEffect, useCallback } from 'react';
+import { useParams } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 import apiConfig from '../apis/apiConfig';
+import Loading from '../../commons/components/Loading';
 
-const WriteContainer = () => {
+const WriteContainer = ({ setPageTitle }) => {
+  const [board, setBoard] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const { bid } = useParams();
+
+  const { t } = useTranslation();
+
+  useEffect(() => {
+    (async () => {
+      try {
+        setLoading(true);
+
+        const data = await apiConfig(bid);
+        setBoard(data); // 게시판 설정 조회
+        setPageTitle(data.bName); // 사이트 제목
+
+        setLoading(false);
+      } catch (err) {
+        console.error(err);
+      }
+    })();
+  }, [bid, setPageTitle]);
+
+  if (loading || !board) {
+    return <Loading />;
+  }
+  console.log(board);
   return <></>;
 };
 
