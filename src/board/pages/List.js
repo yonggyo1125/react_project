9aa032494c96be03ee0c31f2794019e795412b97
yonggyo1125 <yonggyo00@kakao.com>
2024-08-20 09:53:04 +0900

    .

---
+++ src/board/pages/List.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import { Helmet } from 'react-helmet-async';
+import { useTranslation } from 'react-i18next';
+import { OuterBox } from '../../commons/components/LayoutBox';
+import { MainTitle } from '../../commons/components/TitleBox';
+
+const List = () => {
+  const { t } = useTranslation();
+  const [board, setBoard] = useState({
+    bName: '자유게시판',
+  });
+
+  const { bName } = board;
+  return (
+    <>
+      <Helmet>
+        <title>{t(bName)}</title>
+      </Helmet>
+      <OuterBox>
+        <MainTitle>{t(bName)}</MainTitle>
+      </OuterBox>
+    </>
+  );
+};
+
+export default React.memo(List);
