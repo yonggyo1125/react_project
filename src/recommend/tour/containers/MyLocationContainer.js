f6c80c09a1734804dbc3aa4f847d95498cf70aea
yonggyo1125 <yonggyo00@kakao.com>
2024-08-21 08:48:06 +0900

    .

--- src/recommend/tour/containers/MyLocationContainer.js
+++ src/recommend/tour/containers/MyLocationContainer.js
@@ -1,6 +1,8 @@
 /* eslint-disable no-undef */
 import React, { useEffect, useState } from 'react';
 import { apiList } from '../apis/apiInfo';
+import Loading from '../../../commons/components/Loading';
+import KakaoMap from '../../../kakaoapi/KakaoMap';
 
 const MyLocationContainer = () => {
   const [search, setSearch] = useState({
@@ -8,6 +10,8 @@ const MyLocationContainer = () => {
     sigungu: '',
     limit: 100000,
   });
+  const [center, setCenter] = useState([]); // 지도 중심 좌표(현재 위치)
+  const [locations, setLocations] = useState([]); // 마커 표기할 위도, 경도 정보
 
   /* 현재 위치의 시도, 시군구 찾기 S */
   useEffect(() => {
@@ -15,14 +19,17 @@ const MyLocationContainer = () => {
 
     navigator.geolocation.getCurrentPosition((pos) => {
       const { latitude, longitude } = pos.coords;
+      setCenter({ lat: latitude, lng: longitude });
       geocoder.coord2RegionCode(longitude, latitude, (result, status) => {
         if (status === kakao.maps.services.Status.OK) {
           for (const r of result) {
             if (r.region_type === 'H') {
               setSearch((search) => ({
                 ...search,
-                sido: r.region_1depth_name,
-                sigungu: r.region_2depth_name,
+                //sido: r.region_1depth_name,
+                sido: '경기도',
+                //sigungu: r.region_2depth_name,
+                sigungu: '연천군',
               }));
               break;
             }
@@ -30,7 +37,7 @@ const MyLocationContainer = () => {
         }
       });
     });
-  }, [setSearch]);
+  }, [setSearch, setCenter]);
   /* 현재 위치의 시도, 시군구 찾기 E */
 
   useEffect(() => {
@@ -42,14 +49,32 @@ const MyLocationContainer = () => {
     (async () => {
       try {
         const res = await apiList(search);
-        console.log(search, res);
+
+        // 마커 표기 좌표 가공 처리 S
+        if (!res?.items || res?.items?.length === 0) {
+          return;
+        }
+
+        const _locations = res.items
+          .filter((d) => d.latitude && d.longitude)
+          .map((d) => ({
+            lat: d.latitude,
+            lng: d.longitude,
+          }));
+
+        setLocations(_locations);
+        // 마커 표기 좌표 가공 처리 E
       } catch (err) {
         console.error(err);
       }
     })();
   }, [search]);
 
-  return <></>;
+  if (center?.length === 0 || locations?.length === 0) {
+    return <Loading />;
+  }
+
+  return <KakaoMap center={center} marker={locations} />;
 };
 
 export default React.memo(MyLocationContainer);
