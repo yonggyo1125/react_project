d60a117100e1b36c8e937210ffccde499644a361
yonggyo1125 <yonggyo00@kakao.com>
2024-08-22 15:25:57 +0900

    .

--- src/reservation/containers/ReservationContainer.js
+++ src/reservation/containers/ReservationContainer.js
@@ -1,9 +1,31 @@
-import React, { useEffect } from 'react';
-import { apiGet } from '../../recommend/activity/apis/apiInfo';
+import React, { useEffect, useState, useCallback } from 'react';
+import { useParams } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
+import { apiGet } from '../activity/apis/apiInfo';
 import ReservationForm from '../components/ReservationForm';
+import Loading from '../../commons/components/Loading';
 
-const ReservationContainer = () => {
-    
+const ReservationContainer = ({ setPageTitle }) => {
+  const [data, setData] = useState(null);
+  const { seq } = useParams();
+  const { t } = useTranslation();
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const res = await apiGet(seq);
+        setPageTitle(`${res.townName} ${t('예약하기')}`);
+        setData(res);
+      } catch (err) {
+        console.error(err);
+      }
+    })();
+  }, [seq, t, setPageTitle]);
+
+  if (!data) {
+    return <Loading />;
+  }
+
   return <ReservationForm />;
 };
 
