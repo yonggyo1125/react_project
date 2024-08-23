ab9befafe55aba10f3f70be0079b6f6b9a4ae9af
yonggyo1125 <yonggyo00@kakao.com>
2024-08-23 16:06:18 +0900

    .

--- src/commons/contexts/WishListContext.js
+++ src/commons/contexts/WishListContext.js
@@ -17,4 +17,37 @@ const WishListContext = createContext({
   },
 });
 
+export const WishListProvider = ({ children }) => {
+  const [boardWish, setBoardWish] = useState([]);
+  const [restaurantWish, setRestaurantWish] = useState([]);
+  const [tourWish, setTourWish] = useState([]);
+  const [activityWish, setActivityWish] = useState([]);
+  const [festivalWish, setFestivalWish] = useState([]);
+
+  const value = {
+    states: {
+      boardWish,
+      restaurantWish,
+      tourWish,
+      activityWish,
+      festivalWish,
+    },
+    actions: {
+      setBoardWish,
+      setRestaurantWish,
+      setTourWish,
+      setActivityWish,
+      setFestivalWish,
+    },
+  };
+
+  return (
+    <WishListContext.Provider value={value}>
+      {children}
+    </WishListContext.Provider>
+  );
+};
+
+export const { Consumer: WishListConsumer } = WishListContext;
+
 export default WishListContext;
