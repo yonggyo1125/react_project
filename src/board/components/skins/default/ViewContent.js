379343edb6d2b8ada4b01b271705ef752d9df1b6
yonggyo1125 <yonggyo00@kakao.com>
2024-08-26 20:35:20 +0900

    .

--- src/board/components/skins/default/ViewContent.js
+++ src/board/components/skins/default/ViewContent.js
@@ -1,7 +1,41 @@
 import React from 'react';
+import styled from 'styled-components';
+import { useTranslation } from 'react-i18next';
+import { FcDownload } from 'react-icons/fc';
 
-const ViewContent = () => {
-  return <h1>글내용</h1>;
+const Wrapper = styled.div``;
+
+const ViewContent = ({ data }) => {
+  const { t } = useTranslation();
+
+  return (
+    <Wrapper>
+      <div className="subject">{data.subject}</div>
+      <div className="post-info">
+        <div className="left">
+          {data.poster}
+          {data.member && '(' + data.member.email + ')'}
+        </div>
+        <div className="right">
+          IP. {data.ip} / {t('조회수')}. {data.viewCount.toLocaleString()} /
+          {data.createdAt}
+        </div>
+      </div>
+      <div
+        className="contents"
+        dangerouslySetInnerHTML={{ __html: data.content }}
+      ></div>
+      {data?.attachFiles?.length > 0 && (
+        <ul className="downloads">
+          {data.attachFiles.map(({ fileDownloadUrl, fileName }) => (
+            <li key={fileDownloadUrl}>
+              <FcDownload /> <a href={fileDownloadUrl}>{fileName}</a>
+            </li>
+          ))}
+        </ul>
+      )}
+    </Wrapper>
+  );
 };
 
 export default React.memo(ViewContent);
