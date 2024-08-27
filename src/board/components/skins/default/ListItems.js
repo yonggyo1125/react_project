f3ae2bbf1d84797b6109501e3502d873bb11cdf4
yonggyo1125 <yonggyo00@kakao.com>
2024-08-27 11:37:25 +0900

    .

--- src/board/components/skins/default/ListItems.js
+++ src/board/components/skins/default/ListItems.js
@@ -2,28 +2,28 @@ import React from 'react';
 import styled from 'styled-components';
 import { useTranslation } from 'react-i18next';
 
-const Wrapper = styled.ul`
+const Wrapper = styled.ul``;
 
-`;
-
-const ListItem = ({item, className}) => {
-    return <li className={className}></li>
+const ListItem = ({ item, className }) => {
+  return <li className={className}></li>;
 };
 
-const StyledListItem = styled(ListItem)`
+const StyledListItem = styled(ListItem)``;
 
-`;
-
-const NoData = styled.li`
-
-`;
+const NoData = styled.li``;
 
 const ListItems = ({ items }) => {
-    const { t } = useTranslation();
+  const { t } = useTranslation();
 
-  return <Wrapper>
-    {items && items.length > 0 ? items.map(item => <ListItem></ListItem>)}
-  </Wrapper>
+  return (
+    <Wrapper>
+      {items && items.length > 0 ? (
+        items.map((item) => <StyledListItem key={item.seq} item={item} />)
+      ) : (
+        <NoData>{t('조회된_게시글이_없습니다.')}</NoData>
+      )}
+    </Wrapper>
+  );
 };
 
 export default React.memo(ListItems);
