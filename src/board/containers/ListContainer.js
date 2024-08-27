f103a4be247bdcaf6fc1576ab0c5fd0158c917bc
yonggyo1125 <yonggyo00@kakao.com>
2024-08-27 10:32:20 +0900

    .

--- src/board/containers/ListContainer.js
+++ src/board/containers/ListContainer.js
@@ -1,6 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
+import { useParams } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
+import Loading from '../../commons/components/Loading';
+import pagination from '../../commons/components/Pagination';
+import apiConfig from '../apis/apiConfig';
+import { getList } from '../apis/apiBoard';
 
-const ListContainer = () => {
+const ListContainer = ({ setPageTitle }) => {
+  const { bid } = useParams();
+  const [board, setBoard] = useState(null);
+  const [items, setItems] = useState(null);
+  const [pagination, setPagination] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const res1 = await apiConfig(bid);
+        setBoard(res1);
+        
+      } catch (err) {
+        console.error(err);
+      }
+    })();
+  }, [bid]);
+
   return <></>;
 };
 
