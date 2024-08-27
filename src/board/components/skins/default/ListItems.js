1c59824bae767491b1b8c6505725a0d6e8acdb14
yonggyo1125 <yonggyo00@kakao.com>
2024-08-27 11:36:11 +0900

    .

--- src/board/components/skins/default/ListItems.js
+++ src/board/components/skins/default/ListItems.js
@@ -1,7 +1,29 @@
 import React from 'react';
+import styled from 'styled-components';
+import { useTranslation } from 'react-i18next';
 
-const ListItems = () => {
-  return <h1>리스트</h1>;
+const Wrapper = styled.ul`
+
+`;
+
+const ListItem = ({item, className}) => {
+    return <li className={className}></li>
+};
+
+const StyledListItem = styled(ListItem)`
+
+`;
+
+const NoData = styled.li`
+
+`;
+
+const ListItems = ({ items }) => {
+    const { t } = useTranslation();
+
+  return <Wrapper>
+    {items && items.length > 0 ? items.map(item => <ListItem></ListItem>)}
+  </Wrapper>
 };
 
 export default React.memo(ListItems);
