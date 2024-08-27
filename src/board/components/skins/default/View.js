094fff8b2e953b18f1f01ac541df7ef3890c6699
yonggyo1125 <yonggyo00@kakao.com>
2024-08-27 12:27:14 +0900

    .

--- src/board/components/skins/default/View.js
+++ src/board/components/skins/default/View.js
@@ -4,21 +4,21 @@ import ViewContent from './ViewContent';
 import CommentForm from './CommentForm';
 import CommentItems from './CommentItems';
 
-const View = ({board, data, onDelete}) => {
-    const { useComment } = board;
+const View = ({ board, data, onDelete }) => {
+  const { useComment } = board;
 
-    return (
-      <>
-        <ViewContent data={data} onDelete={onDelete} />
-  
-        {useComment && (
-          <>
-            {data.commentable && <CommentForm />}
-            {data?.comments?.length > 0 && <CommentItems items={data.comments} />}
-          </>
-        )}
-      </>
-    );
+  return (
+    <>
+      <ViewContent data={data} onDelete={onDelete} />
+
+      {useComment && (
+        <>
+          {data.commentable && <CommentForm />}
+          {data?.comments?.length > 0 && <CommentItems items={data.comments} />}
+        </>
+      )}
+    </>
+  );
 };
 
 export default React.memo(View);
