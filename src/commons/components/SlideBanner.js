22fc6118b1f7e39078f3ee207622a8ce0fbc38a0
yonggyo1125 <yonggyo00@kakao.com>
2024-08-27 19:38:41 +0900

    .

--- src/commons/components/SlideBanner.js
+++ src/commons/components/SlideBanner.js
@@ -2,10 +2,10 @@ import React from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination } from 'swiper/modules';
 import 'swiper/css';
-import banner1 from '../../images/banner/banner1.png';
-import banner2 from '../../images/banner/banner2.png';
-import banner3 from '../../images/banner/banner3.png';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 
 const Wrapper = styled.div`
   width: ${({ width }) => (width ? width + 'px' : '100%')};
@@ -17,23 +17,37 @@ const Wrapper = styled.div`
   }
 `;
 
-const SlideBanner = ({ items, width, options, onChange, onSwiper }) => {
+const SlideBanner = ({
+  items,
+  width,
+  options,
+  onChange,
+  onSwiper,
+  className,
+}) => {
   options = options ?? {};
   const { spaceBetween, slidesPerView, loop, speed } = options;
+
+  const swiperProps = {
+    modules: [Navigation, Pagination],
+    navigation: true,
+    pagination: { clickable: true },
+    spaceBetween: spaceBetween ?? 0,
+    slidesPerView: slidesPerView ?? 1,
+    loop: Boolean(loop),
+    speed: speed ?? 500,
+    onSlideChange: onChange ? onChange : () => {},
+    onSwiper: onSwiper ? onSwiper : () => {}}
+  };
+
+
   return (
     items &&
     items.length > 0 && (
-      <Wrapper width={width}>
-        <Swiper
-          spaceBetween={spaceBetween ?? 0}
-          slidesPerView={slidesPerView ?? 1}
-          loop={Boolean(loop)}
-          speed={speed ?? 500}
-          onSlideChange={onChange ? onChange : () => {}}
-          onSwiper={onSwiper ? onSwiper : () => {}}
-        >
+      <Wrapper width={width} className={className}>
+        <Swiper {...swiperProps}>
           {items.map(({ image, link, alt }) => (
-            <SwiperSlide className="banner">
+            <SwiperSlide key={image} className="banner">
               {link?.trim() ? (
                 <Link to={link}>
                   <img src={image} alt={alt} />
--- src/main/pages/Main.js
+++ src/main/pages/Main.js
@@ -1,13 +1,33 @@
 import React from 'react';
+import styled from 'styled-components';
 import SlideBanner from '../../commons/components/SlideBanner';
+import banner1 from '../../images/banner/banner1.png';
+import banner2 from '../../images/banner/banner2.png';
+import banner3 from '../../images/banner/banner3.png';
+
+const Wrapper = styled.div`
+  div {
+    margin: 20px auto;
+  }
+`;
 
 const options = {
   loop: true,
   speed: 1000,
 };
 
+const items = [
+  { image: banner1, link: '/board/view/1', alt: '배너1' },
+  { image: banner2, link: '/board/view/2', alt: '배너2' },
+  { image: banner3, link: '/board/view/3', alt: '배너3' },
+];
+
 const Main = () => {
-  return <SlideBanner width={600} options={options} />;
+  return (
+    <Wrapper>
+      <SlideBanner items={items} width={600} options={options} />
+    </Wrapper>
+  );
 };
 
 export default React.memo(Main);
